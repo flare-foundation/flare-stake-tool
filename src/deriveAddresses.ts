9486972103cda42f24484f5164a42b85bf449401
kuco23 <nseverkar@gmail.com>
2022-09-13 14:18:15 +0200

    staking to flare local node

--- src/deriveAddresses.ts
+++ src/deriveAddresses.ts
@@ -1,15 +1,26 @@
-const { protocol, ip, port, networkID, privateKey, cAddressHex } = require("../config.ts")
-import { Avalanche } from "avalanche/dist"
-import { PlatformVMAPI, KeyChain as pKeyChain } from "avalanche/dist/apis/platformvm"
-import { PrivateKeyPrefix } from "avalanche/dist//utils"
+const {
+  protocol,
+  ip,
+  port,
+  networkID,
+  privateKey,
+  cAddressHex,
+} = require('../config.ts')
+import { Avalanche } from 'avalanche/dist'
+import { PrivateKeyPrefix } from 'avalanche/dist//utils'
 
-const privKey: string = `${PrivateKeyPrefix}${privateKey}`
+const privKey = `${PrivateKeyPrefix}${privateKey}`
 
 const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID)
-const pchain: PlatformVMAPI = avalanche.PChain()
-const pKeychain: pKeyChain = pchain.keyChain()
+const pchain = avalanche.PChain()
+const cchain = avalanche.CChain()
+const pKeychain = pchain.keyChain()
+const cKeychain = cchain.keyChain()
 pKeychain.importKey(privKey)
+cKeychain.importKey(privKey)
 const pAddressStrings: string[] = pchain.keyChain().getAddressStrings()
+const cAddressStrings: string[] = cchain.keyChain().getAddressStrings()
 
 console.log(`P-chain address: ${pAddressStrings}`)
-console.log(`C-chain address: ${cAddressHex}`)
+console.log(`C-chain address: ${cAddressStrings}`)
+console.log(`C-chain address hex: ${cAddressHex}`)
