eed50acbb5ffa345d0aa89d72ce0cabd10164a74
kuco23 <nseverkar@gmail.com>
2022-10-11 12:35:57 +0200

    switched to @flarenetwork/flarejs npm dependency

--- test/test.ts
+++
@@ -1,5 +0,0 @@
-import { pchain, pAddressBech32, cAddressHex, cAddressBech32 } from '../src/constants'
-import { parseToID } from '../src/utils'
-import { integerToDecimal } from '../src/utils'
-
-console.log(integerToDecimal("1231", 9))
