2083fdfe7f2bdad7425929c82f71f859ebc09728
akp111 <pradhanashish95@gmail.com>
2023-08-04 20:19:36 +0530

    fixed typo

--- test/unit/utils.test.ts
+++ test/unit/utils.test.ts
@@ -1,6 +1,6 @@
 import { privateKeyToEncodedPublicKey } from "../../src/utils"
 import data from "../data"
-describe("Unit Test Cases for privateKeyToEncodedPublicKey", () => {
+describe("Unit Test Cases for utils", () => {
     describe("privateKeyToEncodedPublicKey Testcases", () => {
         test("Should pass for valid input for privateKey and compres is true", () => {
             const encodedPublicKey = privateKeyToEncodedPublicKey(data.DUMMY_PRIVATE_KEY)
