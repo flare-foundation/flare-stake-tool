393410e7732d29e54037d55dea18b798e21c6651
akp111 <pradhanashish95@gmail.com>
2023-08-04 20:08:41 +0530

    added intitial setup for jest testcases

---
+++ test/data.ts
@@ -0,0 +1,5 @@
+const data = {
+    "DUMMY_PRIVATE_KEY":"d5797b255933f72a6a084fcfc0f5f4881defee8c1ae387197805647d0b10a8a0"
+}
+
+export default data
---
+++ test/unit/utils.test.ts
@@ -0,0 +1,19 @@
+import { privateKeyToEncodedPublicKey } from "../../src/utils"
+import data from "../data"
+describe("Unit Test Cases for privateKeyToEncodedPublicKey", () => {
+    describe("privateKeyToEncodedPublicKey Testcases", () => {
+        test("Should pass for valid input for privateKey and compres is true", () => {
+            const encodedPublicKey = privateKeyToEncodedPublicKey(data.DUMMY_PRIVATE_KEY)
+            expect(typeof encodedPublicKey).toBe("string")
+        })
+        test("Should pass for valid input for privateKey and compress is false", () => {
+            const encodedPublicKey = privateKeyToEncodedPublicKey(data.DUMMY_PRIVATE_KEY, false)
+            expect(typeof encodedPublicKey).toBe("string")
+        })
+        test("Should fail for empty privateKey", () => {
+            expect(() => {
+                privateKeyToEncodedPublicKey('');
+            }).toThrow();
+        })
+    })
+})
