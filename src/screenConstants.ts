1e51a44a87f6513b23f7e9b7e5bfd5d96259a432
Avial Agarwal <aviral@onebyte.ai>
2023-08-08 19:37:22 +0530

    formatting fix

--- src/screenConstants.ts
+++ src/screenConstants.ts
@@ -1,7 +1,7 @@
-export const screenConstants ={
+export const screenConstants = {
     viewChainAddress: 'View chain addresses',
     checkBalance: 'Check on-chain balance',
-    networkInfo : 'Get network info',
+    networkInfo: 'Get network info',
     validatorInfo: 'Get validator info',
     moveCToP: 'Move assets from C-chain to P-chain',
     movePToC: 'Move assets from P-chain to C-chain',
